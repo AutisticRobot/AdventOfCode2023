9656f8c203c4e0660bc16493e9359a50f93b4300
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2023-12-19 23:21:59 -0800

    worked on AOC 2023 Q9

--- Q9/sulution.cpp
+++ Q9/sulution.cpp
@@ -1,8 +1,11 @@
 #include "code.cpp"
+#include <cctype>
 
-int *sor;
-int *des;
+int *sorArr[24];
+int *desArr[24];
+int length = 24;
 int state = 0;
+bool startNum = true;
 
 
 bool processLine()
@@ -14,32 +17,46 @@ bool processLine()
     switch (state)
     {
         case 0:
-        curLine.erase(0,7);
+            curLine.erase(0,7);
         break;
         case 1:
+
+
         break;
 
     }
 
     for(int i=0;curLine.size()>0;i++)
     {
+        std::string tmp;
         switch (state)
         {
             case 0:
+                if(std::isdigit(curLine[0]))
+                {
+                    if(startNum)
+                    {
+                        tmp = firstNum(curLine);
+                        log(firstNum(curLine));
+                        log(' ');
+                        startNum = false;
+                    }
+
+                }else{
+                    startNum = true;
+                }
+                state = 1;
+            break;
+
             case 1:
-            std::string tmp;
-            tmp = firstNum(curLine);
-            log(firstNum(curLine));
-            log('>');
-            //log(curLine[0]);
-            log(',');
-            state = 1;
+
+            break;
 
         }
 
             curLine.erase(0,1);
     }
-
+    log("\n");
     return true;
 }
 
